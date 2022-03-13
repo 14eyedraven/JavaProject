8bdc2d7c660caff77824ae42f232f86eebc4715e
Zamir Osmenai <dit19157@uop.gr>
2022-03-13 02:17:00 +0200

    Class Creation and Unility Importing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+
+        // Create Input Stream and Scanner..
+        FileInputStream fin = new FileInputStream("test.txt");
+        Scanner input = new Scanner(fin);
+
+        // Create ArrayList..
+        ArrayList<WordsFrequency> aWordsFrequencyList = new ArrayList<>();
+
+        // Create variable of that class..
+        WordsFrequency WF = new WordsFrequency();
     }
 }
+
+
+class WordsFrequency {
+
+}
+
+
